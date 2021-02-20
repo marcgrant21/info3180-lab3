455358bf08ef11fb1614556546579112c8a2f1c6
testaa1 <43795666+marcgrant21@users.noreply.github.com>
2021-02-20 17:43:54 -0500

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -7,8 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
-
+from .form import ContactForm
+from app import mail
+from flask_mail import Message
 ###
 # Routing for your application.
 ###
@@ -29,15 +30,22 @@ def about():
 # The functions below should be applicable to all Flask apps.
 ###
 
+@app.route('/contact', methods=["GET","POST"])
+def contact():
+    contactForm = ContactForm()
 
-# Flash errors from the form if validation fails
-def flash_errors(form):
-    for field, errors in form.errors.items():
-        for error in errors:
-            flash(u"Error in the %s field - %s" % (
-                getattr(form, field).label.text,
-                error
-            ), 'danger')
+    if request.method =="POST":
+
+        if contactForm.validate_on_submit():
+            msg = Message(contactForm.subject.data, sender=(contactForm.name.data,contactForm.email.data), recipients=["marcgrant10@gmail.com"])
+            msg.body = contactForm.message.data
+            mail.send(msg)
+
+            flash('Message sent', 'success')
+            return redirect(url_for('home'))
+
+    return render_template("contact.html", contactForm = contactForm)
+
 
 
 @app.route('/<file_name>.txt')
